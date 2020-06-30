a6719962e04c6b64ea891892d94fa9ad2ca5b587
Karan <karangadani@gmail.com>
2020-06-30 22:55:45 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:promotion_ui/provider/loyaltyrules.dart';
+
 import 'package:promotion_ui/screens/promotion_screen.dart';
-import 'package:provider/provider.dart';
+
 
 
 void main() => runApp(MaterialApp(
   theme: ThemeData(primaryColor: Colors.pink),
-  home: ChangeNotifierProvider.value(
-      value: Rules(),
-      child: PromotionScreen()),
+  home: PromotionScreen(),
   ),
 );
--- lib/screens/promotion_screen.dart
+++ lib/screens/promotion_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:promotion_ui/provider/loyaltyrules.dart';
 import 'package:promotion_ui/screens/loyalty_screen.dart';
+import 'package:provider/provider.dart';
 import '../models/food_promo.dart';
 import '../live.dart';
 import '../previous.dart';
@@ -56,58 +58,61 @@ class _PromotionScreenState extends State<PromotionScreen> with SingleTickerProv
     int _currentIndex = 0;
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      backgroundColor: Colors.grey.shade200,
-      body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(height: 20,),
-            SafeArea(
-              child: Padding(
-                padding: EdgeInsets.only(left: 8),
-                child: IconButton(
-                icon: Icon(Icons.menu,size: 30,),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context){
-                    return Loyalty();
-                  }));
-                }
-              ),),
-            ),
-            SizedBox(height: 20,),
-            Padding(
-              padding: EdgeInsets.only(left: 20),
-              child:Text('Promotions',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 28),),),
-            SizedBox(height: 40),
-            TabBar(
-              controller: _tabController,
-              indicatorColor: Colors.transparent,
-              labelColor: Colors.black,
-              isScrollable: true,
-              labelPadding: EdgeInsets.only(right: 35,left: 20),
-              unselectedLabelColor: Colors.grey,
-              tabs: <Widget>[
-                Tab(
-                  child: Text('Live',style: TextStyle(fontSize: 20,)),
-                ),
-                Tab(
-                  child: Text('Previous',style: TextStyle(fontSize: 20,)),
-                ),
-              ],
-            ),
-            Container(
-              height: screenHeight*0.8,
-            child:TabBarView(
-              controller: _tabController,
-              children: <Widget>[
-                Live(foodItemData),
-                Previous(foodItemData),
-              ],
-            ))
-          ],
+    return ChangeNotifierProvider.value(
+      value: Rules(),
+          child: Scaffold(
+        backgroundColor: Colors.grey.shade200,
+        body: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 20,),
+              SafeArea(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 8),
+                  child: IconButton(
+                  icon: Icon(Icons.menu,size: 30,),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context){
+                      return Loyalty();
+                    }));
+                  }
+                ),),
+              ),
+              SizedBox(height: 20,),
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+                child:Text('Promotions',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 28),),),
+              SizedBox(height: 40),
+              TabBar(
+                controller: _tabController,
+                indicatorColor: Colors.transparent,
+                labelColor: Colors.black,
+                isScrollable: true,
+                labelPadding: EdgeInsets.only(right: 35,left: 20),
+                unselectedLabelColor: Colors.grey,
+                tabs: <Widget>[
+                  Tab(
+                    child: Text('Live',style: TextStyle(fontSize: 20,)),
+                  ),
+                  Tab(
+                    child: Text('Previous',style: TextStyle(fontSize: 20,)),
+                  ),
+                ],
+              ),
+              Container(
+                height: screenHeight*0.8,
+              child:TabBarView(
+                controller: _tabController,
+                children: <Widget>[
+                  Live(foodItemData),
+                  Previous(foodItemData),
+                ],
+              ))
+            ],
+          ),
         ),
       ),
     );
